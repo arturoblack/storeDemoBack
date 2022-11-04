2fdf2d3e2b13d1ee1f3c449a4ee6d7223c9bd2bf
Arturo Bolanos <arturoblack0@gmail.com>
2022-11-04 17:58:34 -0500

    add index

---
+++ otroProyecto5/Controllers/indexController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using ReservasDAL.Entities.ReservasDb.tables;
+using ReservasDAL.Services.Products;
+
+namespace otroProyecto5.Controllers
+{
+    [Route("/")]
+    public class indexController: ControllerBase
+    {
+        public indexController()
+        {
+        }
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult GetAllAsync()
+        {
+            return Ok(new {page = "ok"});
+        }
+    }
+}
+
